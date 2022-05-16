da4ed6afd07f72af7d8de9fbdf50e84749bdb469
duyngo0501 <97430123+duyngo0501@users.noreply.github.com>
2022-05-16 10:25:29 +0700

    Commited

---
+++ Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fact = 1, i = 1;
+            do
+            {
+                Console.Write("Enter n to process: ");
+                n = int.Parse(Console.ReadLine());
+
+
+            }
+            while (n < 0);
+            if (n ==0)
+            {
+                Console.WriteLine("Factorial number of {0} is {1} ", n, fact);
+
+            }  
+            else
+            {
+                for (i = 1; i <= n;i++)
+                {
+                    fact = fact * i;
+                }
+                Console.WriteLine("Factorial number of {0} is {1}", n, fact);
+            }
+            Console.ReadKey();
+        }
+    }
+}
